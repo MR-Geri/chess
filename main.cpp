7eecbc61310780d0ab8ef50ab93f3bdf33d0ea50
Pavel Stebenev <pstebeneff@yandex.ru>
2022-09-20 12:33:16 +0300

    Reconfiguration project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "chess.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Chess w;
     w.show();
     return a.exec();
 }
