a3fd5cf75c91dae5507fb3ccdb14ab35c0836ae1
Pavel Stebenev <pstebeneff@yandex.ru>
2022-09-21 09:59:09 +0300

    Smth

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "chess.h"
+#include "settings.h"
+#include "menu.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Chess w;
-    w.show();
+    Chess chess;
+    Menu menu;
+    Settings settings;
+    menu.show();
+
     return a.exec();
 }
