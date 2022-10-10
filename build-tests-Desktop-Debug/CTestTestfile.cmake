f2dfd49f7f0c4648bae5726bc856026b046d861b
NickSpv <91346886+NickSpv@users.noreply.github.com>
2022-10-10 14:43:44 -0400

    smt

--- build-tests-Desktop-Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pavel/GitHub/chess/tests
-# Build directory: /home/pavel/GitHub/chess/build-tests-Desktop-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/pavel/GitHub/chess/build-tests-Desktop-Debug/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/pavel/GitHub/chess/tests/CMakeLists.txt;26;add_test;/home/pavel/GitHub/chess/tests/CMakeLists.txt;0;")
-subdirs("sourse_dir")
--- build-tests-Desktop-Debug/sourse_dir/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pavel/GitHub/chess
-# Build directory: /home/pavel/GitHub/chess/build-tests-Desktop-Debug/sourse_dir
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
