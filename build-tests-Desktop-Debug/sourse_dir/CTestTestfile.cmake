4578199a0796a9fe6031c30907175da1f0983d58
NickSpv <91346886+NickSpv@users.noreply.github.com>
2022-10-10 14:37:52 -0400

    add tests

---
+++ build-tests-Desktop-Debug/sourse_dir/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pavel/GitHub/chess
+# Build directory: /home/pavel/GitHub/chess/build-tests-Desktop-Debug/sourse_dir
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ tests/tst_tests.cpp
@@ -0,0 +1,35 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class tests : public QObject
+{
+    Q_OBJECT
+
+public:
+    tests();
+    ~tests();
+
+private slots:
+    void test_case1();
+
+};
+
+tests::tests()
+{
+
+}
+
+tests::~tests()
+{
+
+}
+
+void tests::test_case1()
+{
+    King h;
+}
+
+QTEST_APPLESS_MAIN(tests)
+
+#include "tst_tests.moc"
