37bc52344e400a4a86c0c94a2143690b57ad5a96
NickSpv <91346886+NickSpv@users.noreply.github.com>
2022-11-01 12:37:02 +0300

    Add StatusMove enum

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,16 @@
 #pragma once
 
-/*!
- * \brief The Windows enum
- * перечисление, содержащее окна приложения.
+/**
+ * \brief Перечисление, содержащее окна приложения.
  */
 enum Windows { GAME_FIELD, MENU, SETTINGS, LIDERBOARD, EXIT };
+
+/**
+ * @brief Перечисление, содержащее значение, возвращаемое функцией move.
+ * \n DONE - перемещение выполнено
+ * \n FAIL - отсутствие перемещения
+ * \n GO_OUT - выход за игровое поле
+ * \n IMPOSSIBLE_MOVE - фигура не может совершить такой ход
+ * \n MOVE_TO_YOUR_FIGURE - ход совершается на/через собственную фигуру
+ */
+enum StatusMove { DONE, FAIL, GO_OUT, IMPOSSIBLE_MOVE, MOVE_TO_YOUR_FIGURE };
--- src/logic/engine/engine.cpp
+++ src/logic/engine/engine.cpp
@@ -4,18 +4,18 @@
 
 Engine::Engine() {}
 
-int Engine::move(QPair<int, int> from_pos, QPair<int, int> step) {
+StatusMove Engine::move(QPair<int, int> from_pos, QPair<int, int> step) {
   QPair<int, int> to_pos(from_pos.first + step.first,
                          from_pos.second + step.second);
   if (step.first == 0 && step.second == 0) {
     // Отсутствие хода
-    return 1;
+    return FAIL;
   }
 
   if (to_pos.first < 0 || to_pos.first >= 8 || to_pos.second < 0 ||
       to_pos.second >= 8) {
     // Выход за поле
-    return 2;
+    return FAIL;
   }
 
   QSet<QPair<int, int>> possible_moves =
@@ -27,17 +27,17 @@ int Engine::move(QPair<int, int> from_pos, QPair<int, int> step) {
        to_pos_figure.getPrice() != 0) ||
       possible_attacks.find(step) == possible_attacks.end()) {
     // Невозможный ход
-    return 3;
+    return IMPOSSIBLE_MOVE;
   }
 
   // V Ошибка, если ход на/через собственную фигуру V
   if (0) {
-    return 4;
+    return MOVE_TO_YOUR_FIGURE;
   }
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   game_board.move(from_pos, to_pos);
-  return 0;
+  return DONE;
 }
 
 int Engine::transform(QPair<int, int> pos) { return 0; }
