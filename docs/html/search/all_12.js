8afdd9608c2dea87062f4b20bf246d5e776bfcfd
Jenkins <ilya.kamckine@yandex.ru>
2022-11-06 12:18:56 +0000

    Jenkins fix: test README.md

--- docs/html/search/all_12.js
+++ docs/html/search/all_12.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['y_91',['y',['../classPosition.html#a82734fc09dabee57a86c6dd31ecdf06c',1,'Position']]]
+  ['y_92',['y',['../classPosition.html#a82734fc09dabee57a86c6dd31ecdf06c',1,'Position']]]
 ];
