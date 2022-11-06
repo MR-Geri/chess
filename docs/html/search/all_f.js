874ba0e88950f579b0f50d25f0e31df2980430f7
Jenkins <ilya.kamckine@yandex.ru>
2022-11-06 12:38:31 +0000

    Jenkins fix: test README.md

--- docs/html/search/all_f.js
+++ docs/html/search/all_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ui_85',['Ui',['../namespaceUi.html',1,'']]],
-  ['update_86',['update',['../classScreenGame.html#afb3deb5d3d6c4e13899e83de7bed5720',1,'ScreenGame']]],
-  ['utils_2eh_87',['utils.h',['../utils_8h.html',1,'']]]
+  ['ui_84',['Ui',['../namespaceUi.html',1,'']]],
+  ['update_85',['update',['../classScreenGame.html#afb3deb5d3d6c4e13899e83de7bed5720',1,'ScreenGame']]],
+  ['utils_2eh_86',['utils.h',['../utils_8h.html',1,'']]]
 ];
