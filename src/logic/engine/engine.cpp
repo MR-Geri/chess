71a112f3810291c2fc257050163855f4851723e9
laptop <ilya.kamckine@ya.ru>
2022-11-22 09:57:36 +0300

    fix position figures

--- src/logic/engine/engine.cpp
+++ src/logic/engine/engine.cpp
@@ -78,17 +78,17 @@ void Engine::setStartingArrangement() {
   game_board->setFigureOn(new Rook(BLACK), {0, 0});
   game_board->setFigureOn(new Rook(WHITE), {7, 7});
   game_board->setFigureOn(new Rook(BLACK), {7, 0});
-  game_board->setFigureOn(new Bishop(WHITE), {1, 7});
-  game_board->setFigureOn(new Bishop(BLACK), {1, 0});
-  game_board->setFigureOn(new Bishop(WHITE), {6, 7});
-  game_board->setFigureOn(new Bishop(BLACK), {6, 0});
-  game_board->setFigureOn(new Knight(WHITE), {2, 7});
-  game_board->setFigureOn(new Knight(BLACK), {2, 0});
-  game_board->setFigureOn(new Knight(WHITE), {5, 7});
-  game_board->setFigureOn(new Knight(BLACK), {5, 0});
-  game_board->setFigureOn(new Queen(WHITE), {4, 7});
+  game_board->setFigureOn(new Bishop(WHITE), {2, 7});
+  game_board->setFigureOn(new Bishop(BLACK), {2, 0});
+  game_board->setFigureOn(new Bishop(WHITE), {5, 7});
+  game_board->setFigureOn(new Bishop(BLACK), {5, 0});
+  game_board->setFigureOn(new Knight(WHITE), {1, 7});
+  game_board->setFigureOn(new Knight(BLACK), {1, 0});
+  game_board->setFigureOn(new Knight(WHITE), {6, 7});
+  game_board->setFigureOn(new Knight(BLACK), {6, 0});
+  game_board->setFigureOn(new Queen(WHITE), {3, 7});
   game_board->setFigureOn(new Queen(BLACK), {3, 0});
-  game_board->setFigureOn(new King(WHITE), {3, 7});
+  game_board->setFigureOn(new King(WHITE), {4, 7});
   game_board->setFigureOn(new King(BLACK), {4, 0});
 }
 
