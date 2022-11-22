9cd8f118e82522df96df47fbd23d881165731b33
laptop <ilya.kamckine@ya.ru>
2022-11-22 10:01:37 +0300

    fix position figures

--- tests/tst_engine.cpp
+++ tests/tst_engine.cpp
@@ -13,20 +13,20 @@ void TestsEngine::test_setStartingArrangement() {
   QCOMPARE(engine.getData()[7][7]->getTypeFigure(), W_ROOK);
   QCOMPARE(engine.getData()[7][0]->getTypeFigure(), B_ROOK);
 
-  QCOMPARE(engine.getData()[1][7]->getTypeFigure(), W_BISHOP);
-  QCOMPARE(engine.getData()[1][0]->getTypeFigure(), B_BISHOP);
-  QCOMPARE(engine.getData()[6][7]->getTypeFigure(), W_BISHOP);
-  QCOMPARE(engine.getData()[6][0]->getTypeFigure(), B_BISHOP);
+  QCOMPARE(engine.getData()[2][7]->getTypeFigure(), W_BISHOP);
+  QCOMPARE(engine.getData()[2][0]->getTypeFigure(), B_BISHOP);
+  QCOMPARE(engine.getData()[5][7]->getTypeFigure(), W_BISHOP);
+  QCOMPARE(engine.getData()[5][0]->getTypeFigure(), B_BISHOP);
 
-  QCOMPARE(engine.getData()[2][7]->getTypeFigure(), W_KNIGHT);
-  QCOMPARE(engine.getData()[2][0]->getTypeFigure(), B_KNIGHT);
-  QCOMPARE(engine.getData()[5][7]->getTypeFigure(), W_KNIGHT);
-  QCOMPARE(engine.getData()[5][0]->getTypeFigure(), B_KNIGHT);
+  QCOMPARE(engine.getData()[1][7]->getTypeFigure(), W_KNIGHT);
+  QCOMPARE(engine.getData()[1][0]->getTypeFigure(), B_KNIGHT);
+  QCOMPARE(engine.getData()[6][7]->getTypeFigure(), W_KNIGHT);
+  QCOMPARE(engine.getData()[6][0]->getTypeFigure(), B_KNIGHT);
 
-  QCOMPARE(engine.getData()[4][7]->getTypeFigure(), W_QUEEN);
+  QCOMPARE(engine.getData()[3][7]->getTypeFigure(), W_QUEEN);
   QCOMPARE(engine.getData()[3][0]->getTypeFigure(), B_QUEEN);
 
-  QCOMPARE(engine.getData()[3][7]->getTypeFigure(), W_KING);
+  QCOMPARE(engine.getData()[4][7]->getTypeFigure(), W_KING);
   QCOMPARE(engine.getData()[4][0]->getTypeFigure(), B_KING);
 
   for (int i = 0; i < 8; i++) {
