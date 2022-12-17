54bcefffa4880821f25845d967de324da978a808
laptop <ilya.kamckine@ya.ru>
2022-12-17 14:52:11 +0300

    add storage

--- src/storage/settings/settings.cpp
+++ src/storage/settings/settings.cpp
@@ -1,44 +1,12 @@
 #include "settings.h"
 
-struct Book {
-  std::string name;
-  double price;
-  Book(std::string _name, double _price) {
-    name = _name;
-    price = _price;
-  }
-};
-
-void CreateJson(const QString &path) {
-  Book obj1("high school mathematics", 12);
-  QJsonObject book1;
-  book1.insert("name", obj1.name.c_str());
-  book1.insert("price", obj1.price);
-
-  Book obj2("advanced high school mathematics", 14);
-  QJsonObject book2;
-  book2.insert("name", obj2.name.c_str());
-  book2.insert("price", obj2.price);
-
-  QJsonObject content;
-  content.insert("book1", book1);
-  content.insert("book2", book2);
-
-  QJsonDocument document;
-  document.setObject(content);
-  QByteArray bytes = document.toJson(QJsonDocument::Indented);
-  QFile file(path);
-  if (file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate)) {
-    QTextStream iStream(&file);
-    iStream.setCodec("utf-8");
-    iStream << bytes;
-    file.close();
-  } else {
-    std::cout << "file open failed: " << path.toStdString() << std::endl;
-  }
+Settings::Settings(){
+  load();
+  save();
 }
 
-void ReadJson(const QString &path) {
+void Settings::load(){
+  std::cout << "LOAD\n";
   QFile file(path);
   if (file.open(QIODevice::ReadOnly)) {
     QByteArray bytes = file.readAll();
@@ -53,30 +21,49 @@ void ReadJson(const QString &path) {
     }
     if (document.isObject()) {
       QJsonObject jsonObj = document.object();
-      QStringList books;
-      books << "book1"
-            << "book2";
-      for (auto book : books) {
-        if (jsonObj.contains(book)) {
-          QJsonObject obj = jsonObj.value(book).toObject();
-          QStringList keys = obj.keys();
-          for (auto key : keys) {
-            auto value = obj.take(key);
-            if (value.isDouble()) {
-              qDebug() << key << " : " << value.toDouble();
-            } else if (value.isString()) {
-              qDebug() << key << " : " << value.toString();
-            }
-          }
-        }
-      }
+      // QStringList books;
+      m_music_flag = jsonObj.take("music_flag").toBool();
+      m_music_volume = jsonObj.take("music_volume").toInt();
+      m_sound_flag = jsonObj.take("sound_flag").toBool();
+      m_sound_volume = jsonObj.take("sound_volume").toInt();
+      // books << "book1"
+      //       << "book2";
+      // for (auto book : books) {
+      //   if (jsonObj.contains(book)) {
+      //     QJsonObject obj = jsonObj.value(book).toObject();
+      //     QStringList keys = obj.keys();
+      //     for (auto key : keys) {
+      //       auto value = obj.take(key);
+      //       if (value.isDouble()) {
+      //         qDebug() << key << " : " << value.toDouble();
+      //       } else if (value.isString()) {
+      //         qDebug() << key << " : " << value.toString();
+      //       }
+      //     }
+      //   }
+      // }
     }
   }
 }
 
-int testJson() {
-  QString path = "data/tests.json";
-  CreateJson(path);
-  ReadJson(path);
-  return 0;
+void Settings::save(){
+  std::cout << "SAVE\n";
+  QJsonObject settings;
+  settings.insert("music_flag", m_music_flag);
+  settings.insert("music_volume", m_music_volume);
+  settings.insert("sound_flag", m_sound_flag);
+  settings.insert("sound_volume", m_sound_volume);
+
+  QJsonDocument document;
+  document.setObject(settings);
+  QByteArray bytes = document.toJson(QJsonDocument::Indented);
+  QFile file(path);
+  if (file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate)) {
+    QTextStream iStream(&file);
+    iStream.setCodec("utf-8");
+    iStream << bytes;
+    file.close();
+  } else {
+    std::cout << "file open failed: " << path.toStdString() << std::endl;
+  }
 }
--- src/storage/settings/settings.h
+++ src/storage/settings/settings.h
@@ -3,3 +3,31 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <iostream>
+
+
+class Settings {
+public:
+  Settings();
+
+  int getMusicVolume();
+  bool getMusicFlag();
+  int getSoundVolume();
+  bool getSoundFlag();
+
+public slots:
+  void changeMusicVolume(int value);
+  void changeSoundVolume(int value);
+  void changeMusicFlag(bool flag);
+  void changeSoundFlag(bool flag);
+
+private:
+  QString path="./data/settings.json";
+
+  int m_music_volume;
+  bool m_music_flag;
+  int m_sound_volume;
+  bool m_sound_flag;
+
+  void save();
+  void load();
+};
